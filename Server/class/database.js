ae6217cb1f19a73725fcb4768ae0cbfa96882e61
PreCodeEU <germerott@precode.eu>
2018-10-27 14:47:19 +0200

    Design of first page

--- Server/class/database.js
+++ Server/class/database.js
@@ -1,10 +1,15 @@
 const mongoose = require('mongoose');
+//Connect to the database
 mongoose.connect('mongodb://localhost:27017/lual', {
+    //Use the new url parser cause the old one is deprecated
     useNewUrlParser: true
 });
+//Userschema
 const User = mongoose.model('User', require(__dirname + '/user'));
+//Logschema
 const Log = mongoose.model('Log', {logString: String});
 class database {
+    //Method to get a user by the username
     static getUser(username){
         User.findOne()
     }
@@ -14,13 +19,17 @@ class database {
     static deleteUser(username){
 
     }
+    //Method to insert a new user
     static insertUser(user){
         let _user = new User(user);
+        //save it
         _user.save(err => {
             if (err) console.log(err);
+            //if its successful, log in the log collection
             else this.insertLog("User " + user.username + " successfuly created!");
         });
     }
+    //Create a new log in the log collection
     static insertLog(logString){
         let _log = new Log({ logString: logString });
         _log.save(err => {
---
+++ Server/views/imports/imports.ejs
@@ -0,0 +1,1 @@
+<script type="text/javascript" src="js/materialize.min.js"></script>
