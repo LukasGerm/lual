e73e90700ee517ae5e1606324aa4db2801dc210f
PreCodeEU <germerott@precode.eu>
2018-10-28 18:37:59 +0100

    Some changes in order

--- Server/public/js/logs.js
+++ Server/public/js/logs.js
@@ -2,15 +2,13 @@ $(document).ready(function(){
     $('.sidenav').sidenav();
     //Getrequest to get the logs
     $.get('/api/getlogs', (data,status) => {
-        console.log(1);
-        $('.preloader-background').fadeOut('slow');
-        $('.loader').fadeOut('slow');
-        //Code goes here
         let logs = JSON.parse(data);
         logs.forEach(element => {
-            //create the log
+            //create the log elements
             document.getElementById('logs').innerHTML += '<tr><td>'+element.date+'</td><td>'+element.logString+'</td></tr>'
         });
-        
+        $('.preloader-background').fadeOut('slow');
+        $('.loader').fadeOut('slow');
+  
     });
   });
