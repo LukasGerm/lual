c51af9e6499fc8006e4c26a015605a0bcd4db1b3
PreCodeEU <germerott@precode.eu>
2018-10-31 12:18:36 +0100

    Replaced attribute

--- Server/public/js/logs.js
+++ Server/public/js/logs.js
@@ -1,7 +1,7 @@
 let currentLogPage = 0;
 let logFilter = null;
 let logPageCount = 0;
-const logPageSize = 2;
+const logPageSize = 10;
 
 //Function for getting the logs from the server
 //The filter argument is optional
@@ -52,12 +52,12 @@ function rebuildPaginationButtons() {
     if (leftButtonDisabled) {
         paginationButtons += '<li class="disabled"><a href="#!"><i class="material-icons">chevron_left</i></a></li>';
     } else {
-        paginationButtons += '<li class="waves-effect"><a href="#!" (click)="setPage(' + (currentLogPage - 1) + ')"><i class="material-icons">chevron_left</i></a></li>';
+        paginationButtons += '<li class="waves-effect"><a href="#!" onclick="setPage(' + (currentLogPage - 1) + ')"><i class="material-icons">chevron_left</i></a></li>';
     }
     if (rightButtonDisabled) {
         paginationButtons += '<li class="disabled"><a href="#!"><i class="material-icons">chevron_right</i></a></li>';
     } else {
-        paginationButtons += '<li class="waves-effect"><a href="#!" (click)="setPage(' + (currentLogPage + 1) + ')"><i class="material-icons">chevron_right</i></a></li>';
+        paginationButtons += '<li class="waves-effect"><a href="#!" onclick="setPage(' + (currentLogPage + 1) + ')"><i class="material-icons">chevron_right</i></a></li>';
     }
     document.getElementById('pagination-container').innerHTML = paginationButtons;
 }
