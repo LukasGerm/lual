fb820d7bcb953994d379b1a1987c7748811f4105
Marcel Andree <marcel@andree.cloud>
2018-11-13 23:23:54 +0100

    Add constants

--- Client/src/main/java/github/lual/Main.java
+++ Client/src/main/java/github/lual/Main.java
@@ -1,19 +1,28 @@
 package github.lual;
 
+import github.lual.util.ComponentManager;
 import javafx.application.Application;
 import javafx.stage.Stage;
 
 public class Main extends Application {
 
+    private static final int WINDOW_WIDTH = 500;
+    private static final int WINDOW_HEIGHT = 500;
+    private static final String WINDOW_TITLE = "Lual";
+
     public static void main(String[] args) {
-        System.out.println("Welcome to Lual.");
         Application.launch(args);
     }
 
     @Override
     public void start(Stage stage) throws Exception {
-        stage.setWidth(500);
-        stage.setHeight(500);
+        final ComponentManager componentManager = new ComponentManager(stage);
+        stage.setWidth(WINDOW_WIDTH);
+        stage.setHeight(WINDOW_HEIGHT);
+        stage.setTitle(WINDOW_TITLE);
+
+        // TODO: Show initial UI component
+
         stage.show();
     }
 }
