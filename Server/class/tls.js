d3adfb345ad16baa8585cc0039fa8b8bcc1202aa
PreCodeEU <germerott@precode.eu>
2018-11-15 14:34:13 +0100

    Some error handling

--- Server/class/tls.js
+++ Server/class/tls.js
@@ -1,35 +1,43 @@
-const tls = require('tls');
-
-
+const tls = require("tls");
 
 //Server for the tls connections
-class tlsServer{
-    constructor(key, cert){
-        //initialize the clients array the sockets will be pushed here
-        this.clients = [];
-        //Tls certs
-        this.options = {
-            key: key,
-            cert: cert
-        }
+class tlsServer {
+  constructor(key, cert) {
+    //initialize the clients array the sockets will be pushed here
+    this.clients = [];
+    //Tls certs
+    this.options = {
+      key: key,
+      cert: cert
+    };
+  }
+  run(port) {
+    let server;
+    server = tls.createServer(this.options, socket => {
+      //Push the client in the client array
+      //Here goes some authentication logic first
+      //Only testing code
+      this.clients.push(socket);
+      socket.setEncoding("utf8");
+      socket.write("welcome<EOF>\n");
+      console.log(this.clients.length);
 
-    }
-    run(port){
-        let server = tls.createServer(this.options, (socket) => {
-            //Push the client in the client array
-            //Here goes some authentication logic first
-            //Only testing code
-            this.clients.push(socket);
-            socket.setEncoding('utf8');
-            socket.write("welcome<EOF>\n");
-        });
-
-        try {
-            server.listen(port, () => {});
-        } catch (error) {   
-            console.log("Connection closed to client");
-        }
-    }
-};
+      //If an error occurs
+      socket.on('error', (e) => {
+          console.log("Connection reset");
+          //Destroy the socket
+          socket.destroy();
+          //Delete it from the array
+          this.clients.forEach(client => {
+              if(client === socket){
+                  client = null;
+              }
+          })
+          console.log(this.clients);
+      })
+    });
+    server.listen(port, () => {});
+  }
+}
 
 module.exports = tlsServer;
