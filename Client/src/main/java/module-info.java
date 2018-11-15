aa153bdd835ef26a808b6121a4fd45b90ffb10d9
Marcel Andree <marcel@andree.cloud>
2018-11-15 18:04:43 +0100

    Import JSerialComm Module

--- Client/src/main/java/module-info.java
+++ Client/src/main/java/module-info.java
@@ -1,6 +1,7 @@
 module lual.client {
     requires javafx.controls;
     requires javafx.fxml;
+    requires com.fazecast.jSerialComm;
     requires static lombok;
     opens github.lual to javafx.graphics;
 }
