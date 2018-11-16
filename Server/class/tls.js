dbdd15f6a9cfe781f74008e9ff0566dba6bb6509
PreCodeEU <germerott@precode.eu>
2018-11-16 08:02:21 +0100

    JWT logic to generate a key and put it back to the client

--- Server/class/tls.js
+++ Server/class/tls.js
@@ -1,5 +1,6 @@
 const tls = require("tls");
 const database = require("./database");
+const jwt = require('jsonwebtoken');
 //Server for the tls connections
 class tlsServer {
   constructor(key, cert) {
@@ -11,6 +12,18 @@ class tlsServer {
       cert: cert
     };
   }
+  //Used to generate the jwt token returns the token
+  generateToken(user, callback){
+    //Sign the token, put the username and password in it
+    jwt.sign({
+      userId: user._id,
+      password: user.password
+    },this.options.key, (err, token) =>{
+      //Return it to the callback
+      if(err) return callback();
+      callback(token);
+    });
+  }
   //Validate the data
   validateData(data, socket) {
     //Split by |
@@ -21,6 +34,21 @@ class tlsServer {
       // First Login case. The user authenticates by username and hashed password. He gets a jwt token and the object-id back so the client can save it
       //If the user loggs in first on the client, the user only has to log in once. If the user logs in first time in general, he gets the popup to change his pw
       case 1:
+      let username = splitData[1];
+      let password = splitData[2];
+      database.getUser(username, (user) => {
+        //Number 1 is wrong pw
+        if(user.password !== password) return socket.write(1);
+        if(user.firstLogin){
+          //Number 2  is first login, indicates that the client opens the changepw form
+          return socket.write(2)
+        }
+        //generate a token and send it to the client
+        this.generateToken(user, (token)=>{
+          if(token) socket.write(token);
+          else console.log('Error signing token');
+        });
+      });
         break;
       // Normal Login case. The client authenticates itself by sending the objectid and the jwt token. The Server checks it, if it is expired, it sends back a message.
       // If not, the user can log in
