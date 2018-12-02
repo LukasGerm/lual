4b982d0d80a7b2a711ebfbe35f33b28cc48737a1
Marcel Andree <marcel@andree.cloud>
2018-12-02 13:30:49 +0100

    Add test alarm

--- Client/src/main/java/github/lual/view/MainView.java
+++ Client/src/main/java/github/lual/view/MainView.java
@@ -1,10 +1,17 @@
 package github.lual.view;
 
 import com.google.common.eventbus.EventBus;
+import com.google.common.eventbus.Subscribe;
+import github.lual.Configuration;
 import github.lual.messages.types.ClientAlarmMessage;
+import github.lual.messages.types.ServerAlarmMessage;
+import github.lual.util.ResourceLoader;
 import github.lual.util.Scene;
+import javafx.application.Platform;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
+
+import java.util.ResourceBundle;
 
 @Scene("main.fxml")
 public class MainView extends BaseView {
@@ -20,6 +27,14 @@ public class MainView extends BaseView {
     public void initialize() {
         btnAlarm.setOnAction(event -> {
             getEventBus().post(new ClientAlarmMessage());
+        });
+    }
+
+    @Subscribe
+    private void onAlarm(ServerAlarmMessage message) {
+        ResourceBundle resourceBundle = ResourceLoader.getInstance().getResourceBundle(Configuration.getInstance().getResourceBundleLanguage());
+        Platform.runLater(() -> {
+            Alerts.info(resourceBundle.getString("AlarmTitle"), String.format(resourceBundle.getString("AlarmText"), message.getFirstName(), message.getLastName(), message.getRoom()), false);
         });
     }
 }
