fe337c28932804c0ca85ea9249a405bb5cfa3d07
Marcel Andree <marcel@andree.cloud>
2018-12-04 20:26:15 +0100

    Improve debugging

--- Client/src/main/java/github/lual/Main.java
+++ Client/src/main/java/github/lual/Main.java
@@ -10,11 +10,14 @@ import github.lual.view.LoginView;
 import github.lual.view.MainView;
 import github.lual.view.ShowComponentEvent;
 import javafx.application.Application;
+import javafx.application.Platform;
 import javafx.stage.Stage;
 
 import java.io.IOException;
 import java.math.BigInteger;
 import java.util.ResourceBundle;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
 public class Main extends Application {
 
@@ -49,7 +52,7 @@ public class Main extends Application {
         stage.show();
 
         // connect the client
-        try {
+        /*try {
             client.connect();
         } catch (Exception e) {
             Alerts.exception(e);
@@ -64,7 +67,7 @@ public class Main extends Application {
                 client.close();
             } catch (IOException e) {
             }
-        });
+        });*/
     }
 
     private void loadComponents(EventBus eventBus) {
@@ -73,29 +76,42 @@ public class Main extends Application {
     }
 
     private void jSerialTest() {
-        SerialPort[] serialPorts = SerialPort.getCommPorts();
-        if (serialPorts.length < 1) {
-            Alerts.error("No serial port", "No serial port found.", false);
-            return;
-        }
-        for (int i = 0; i < serialPorts.length; i++) {
-            Alerts.info("COM-Port " + i, "COM-Port Index" + i + " => " + serialPorts[i].getDescriptivePortName() + " / " + serialPorts[i].getPortDescription() + " / " + serialPorts[i].getSystemPortName(), false );
-        }
-        SerialPort serialPort = serialPorts[0];
-        serialPort.openPort();
-        try {
-            while(true) {
-                while (serialPort.bytesAvailable() <= 0) {
-                    Thread.sleep(20);
-                }
-
-                byte[] readBuffer = new byte[serialPort.bytesAvailable()];
-                int numRead = serialPort.readBytes(readBuffer, readBuffer.length);
-                String hex = String.format("%040x", new BigInteger(1, readBuffer));
-                Alerts.info("SerialPort result", hex, false);
+        ExecutorService executorService = Executors.newFixedThreadPool(1);
+        executorService.execute(() -> {
+            SerialPort[] serialPorts = SerialPort.getCommPorts();
+            if (serialPorts.length < 1) {
+                Platform.runLater(() -> {
+                    Alerts.error("No serial port", "No serial port found.", false);
+                });
+                return;
             }
-        } catch (Exception e) {
-            Alerts.exception(e);
-        }
+            for (int i = 0; i < serialPorts.length; i++) {
+                final int ix = i;
+                Platform.runLater(() -> {
+                    Alerts.info("COM-Port " + ix, "COM-Port Index" + ix + " => " + serialPorts[ix].getDescriptivePortName() + " / " + serialPorts[ix].getPortDescription() + " / " + serialPorts[ix].getSystemPortName(), false );
+                });
+            }
+            SerialPort serialPort = serialPorts[0];
+            serialPort.openPort();
+            try {
+                while(true) {
+                    int bytesAvailable = serialPort.bytesAvailable();
+                    while (bytesAvailable <= 0) {
+                        Thread.sleep(20);
+                    }
+
+                    byte[] readBuffer = new byte[bytesAvailable];
+                    int numRead = serialPort.readBytes(readBuffer, readBuffer.length);
+                    String hex = String.format("%040x", new BigInteger(1, readBuffer));
+                    Platform.runLater(() -> {
+                        Alerts.info("SerialPort result", hex, false);
+                    });
+                }
+            } catch (Exception e) {
+                Platform.runLater(() -> {
+                    Alerts.exception(e);
+                });
+            }
+        });
     }
 }
